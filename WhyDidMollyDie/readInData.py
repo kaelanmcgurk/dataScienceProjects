452c49c21396b45b5c835fe283ff9fa2bdf0a381
ZELDA\kaelanr <mcg16003@byui.edu>
2020-12-08 17:17:29 -0700

    Cleaned up the .csv a little more. Made numbers into floats

--- WhyDidMollyDie/readInData.py
+++ WhyDidMollyDie/readInData.py
@@ -10,9 +10,34 @@ MollyRun = dat.query("Episode == 'C2E001' | Episode == 'C2E002'| Episode == 'C2E
 MollyRun = MollyRun.replace('Nat20',20)
 MollyRun = MollyRun.replace('Nat1',1)
 MollyRun = MollyRun.replace('Unknown',0)
+MollyRun = MollyRun.replace('Nat6',6)
+MollyRun = MollyRun.replace('Nat5',5)
+MollyRun = MollyRun.replace('Nat13',13)
+MollyRun = MollyRun.replace('Nat14',14)
+MollyRun = MollyRun.replace('Nat4',4)
+MollyRun = MollyRun.replace('Nat8',8)
+MollyRun = MollyRun.replace('20-Feb','C2E020')
+MollyRun = MollyRun.replace('21-Feb','C2E021')
+MollyRun = MollyRun.replace('22-Feb','C2E022')
+MollyRun = MollyRun.replace('23-Feb','C2E023')
+MollyRun = MollyRun.replace('24-Feb','C2E024')
+MollyRun = MollyRun.replace('25-Feb','C2E025')
+MollyRun = MollyRun.replace('26-Feb','C2E026')
 # %%
 colDictionary = {'Total Value': float,
                  'Natural Value': float}
 # %%
 MollyRun = MollyRun.astype(colDictionary)
+# %%
+MollyRun = (MollyRun
+.rename(columns = {"Total Value": "TotalValue",
+"Natural Value": "NaturalValue",
+"Damage Dealt": "DamageDealt"}))
+
+# %%
+MollyRun = MollyRun.assign(DamageDealt = lambda x: x['DamageDealt'].str.extract('(\d+)'))
+
+
+# %%
+MollyRun = MollyRun.astype({'DamageDealt':float})
 # %%

feee1657b306bd411b5e7cfd802727640798c1f3
kaelanmcgurk <mcg16003@byui.edu>
2021-09-18 19:28:18 -0600

    more data

--- ExandriaRolls/howLowAreTheRolls.py
+++ ExandriaRolls/howLowAreTheRolls.py
@@ -5,4 +5,13 @@ import altair as alt
 
 # %%
 ExandriaEp1 = pd.read_csv('All Rolls - ExU - ExU-1.csv')
+ExandriaEp2 = pd.read_csv('All Rolls - ExU - ExU-2.csv')
+ExandriaEp3 = pd.read_csv('All Rolls - ExU - ExU-3.csv')
+ExandriaEp4 = pd.read_csv('All Rolls - ExU - ExU-4.csv')
+ExandriaEp5 = pd.read_csv('All Rolls - ExU - ExU-5.csv')
+ExandriaEp6 = pd.read_csv('All Rolls - ExU - ExU-6.csv')
+ExandriaEp7 = pd.read_csv('All Rolls - ExU - ExU-7.csv')
+ExandriaEp8 = pd.read_csv('All Rolls - ExU - ExU-8.csv')
+
 # %%
+

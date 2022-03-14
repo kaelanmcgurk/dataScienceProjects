95c04d9af7277d03918e80c023322969fd15d2e9
kaelanmcgurk <mcg16003@byui.edu>
2022-03-13 21:06:04 -0600

    Put the datasets together

--- MarchMadness2022/MarchMaddness2022.py
+++ MarchMadness2022/MarchMaddness2022.py
@@ -4,4 +4,15 @@ import altair as alt
 
 # %%
 basketballData = pd.read_csv('cbb.csv')
+basketballData20 = pd.read_csv('cbb20.csv')
+basketballData21 = pd.read_csv('cbb21.csv')
+# %%
+basketballData21['YEAR'] = 2021
+basketballData20['YEAR'] = 2020
+
+# %%
+frames = [basketballData, basketballData20, basketballData21]
+
+basketball_college = pd.concat(frames)
+basketball_college.to_csv(r"C:\Users\kaela\Documents\dataScienceProjects\dataScienceProjects\MarchMadness2022\basketball.csv")
 # %%
